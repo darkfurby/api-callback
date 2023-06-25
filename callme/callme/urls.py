9033936d23835a8494c368da12ce1df1895c64e3
Oliver Mackiewicz <oliver.mackiewicz@gmail.com>
2023-06-25 13:04:56 +0200

    Docker changes (#1)
    * gitignore dockerfile creation build successfull
    
    * dockerfile creation docker-compose, requirements

--- callme/callme/urls.py
+++ callme/callme/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from api.views import receive_url
 
 urlpatterns = [
-    path('receive_url/', receive_url, name='receive_url'),
-    path('receive_url', receive_url, name='receive_url'),
+    path('callme/', receive_url, name='receive_url'),
+    path('callme', receive_url, name='receive_url'),
 ]
